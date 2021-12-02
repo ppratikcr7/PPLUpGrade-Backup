439ba9e1f5872b33ae9510795017bb4a429ec783
pratik <ppratik.cr7@gmail.com>
2021-12-02 14:00:59 +0530

    testing dev deployment of frontend and backend

--- backend/packages/Upgrade/src/api/controllers/AnalyticsController.ts
+++ backend/packages/Upgrade/src/api/controllers/AnalyticsController.ts
@@ -7,6 +7,7 @@ import { EnrollmentDetailAnalyticsValidator } from './validators/EnrollmentDetai
 import { DataExportValidator } from './validators/DataExportValidator';
 import { Logger, LoggerInterface } from '../../decorators/Logger';
 
+
 /**
  * @swagger
  * tags:
